6595ccbb3aff046779632407c2abee01f04a87d8
Hyan <hyanmarcos15@gmail.com>
2023-06-01 20:49:16 -0300

    upload exercício

---
+++ Modulo F - Avançando em Java Script/script.js
@@ -0,0 +1,35 @@
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar(){
+
+    if(isNumero(num.value) && inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')    
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+    } else {
+        alert('Valor inválido ou já encontrado na lista.')        
+    }
+
+    
+}
